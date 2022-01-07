771d13c0c145e331907cd582ab6b6f3f90206350
saikut10 <prosonjitroy99@gmail.com>
2022-01-07 13:06:50 +0600

    first commit

---
+++ admin/manage-admin.php
@@ -0,0 +1,53 @@
+<?php include('partials/menu.php'); ?>
+
+        <!-- Main Content Section Starts -->
+        <div class="main-content">
+            <div class="wrapper">
+                <h1>Manage Admin</h1>
+
+                <table class="tbl-full">
+                    <tr>
+                        <th>S.N.</th>
+                        <th>Full Name</th>
+                        <th>Username</th>
+                        <th>Actions</th>
+                    </tr>
+
+                    <tr>
+                        <td>1. </td>
+                        <td>Pro</td>
+                        <td>Roy</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>2. </td>
+                        <td>Pro</td>
+                        <td>Roy</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>3. </td>
+                        <td>Pro</td>
+                        <td>Roy</td>
+                        <td>
+                            Update Admin
+                            Delete Admin
+                        </td>
+                    </tr>
+                </table>
+
+                <div class="clearfix"></div>
+
+            </div>
+        </div>
+        <!-- Main Content Section Ends -->
+
+<?php include('partials/footer.php'); ?>
